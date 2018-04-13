372dd0d945830c06cbfc96be58be189c7d51c6b5
OdaShow <shota.oda+github@bizreach.co.jp>
2018-04-13 12:49:27 +0900

    [carbon-authentication] add proper exception

---
+++ carbon-authentication/src/main/java/org/carbon/authentication/exception/AuthenticationException.java
@@ -0,0 +1,25 @@
+package org.carbon.authentication.exception;
+
+/**
+ * @author Shota.Oda 2018/02/25.
+ */
+public class AuthenticationException extends Exception {
+    public AuthenticationException() {
+    }
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AuthenticationException(Throwable cause) {
+        super(cause);
+    }
+
+    public AuthenticationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
