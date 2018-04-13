112fa755db90dd8b40ff4b1f613f3154b03a74a1
OdaShow <shota.oda+github@bizreach.co.jp>
2018-04-13 19:13:06 +0900

    [carbon-infrastructure] add sample deploy script

--- carbon-infrastructure/scripts/deploy.prd.sh
+++ carbon-infrastructure/scripts/deploy.prd.sh
@@ -2,33 +2,26 @@
 
 cd `dirname $0`
 
-PORT=44422
-IDENTITY='~/.ssh/local/tmp'
 ROOT_DIR='../..'
 
 # build
 echo '--------------------------------------------------'
 echo 'Build'
 echo '--------------------------------------------------'
-mvn clean package -f $ROOT_DIR/carbon-reactor-v2/pom.xml
+mvn clean package -f ${ROOT_DIR}/carbon-reactor-v2/pom.xml
 
 # copy uber jar
 echo '--------------------------------------------------'
 echo 'Copy uber-jar'
 echo '--------------------------------------------------'
 scp \
--P $PORT \
--i $IDENTITY \
-$ROOT_DIR/carbon-sample-v2/target/carbon-app.jar \
-ansible@localhost:/var/webapp
+${ROOT_DIR}/carbon-sample-v2/target/carbon-app.jar \
+cbn.ggl.ansible:/var/webapp
 
 # run webapp as user[carbon]
-CMD='java -jar -Dprofile=stg /var/webapp/carbon-app.jar'
 echo '--------------------------------------------------'
 echo 'Run webapp as user[carbon]'
-echo $CMD
 echo '--------------------------------------------------'
 ssh \
--p $PORT \
--i $IDENTITY \
-ansible@localhost "sudo runuser - carbon -s /bin/bash -c '$CMD'"
+cbn.ggl.ansible \
+"sudo runuser - carbon -s /bin/bash -c '/etc/init.d/webapp restart'"
